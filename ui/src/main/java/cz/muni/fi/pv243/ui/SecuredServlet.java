b32ab0e64703a48f8316019d5fbb038f99073c87
Holdo <holic.michal@gmail.com>
2016-05-03 15:32:03 +0200

    Initialized role dispatching + fixed some maven stuff

--- ui/src/main/java/cz/muni/fi/pv243/ui/SecuredServlet.java
+++ ui/src/main/java/cz/muni/fi/pv243/ui/SecuredServlet.java
@@ -1,6 +1,7 @@
 package cz.muni.fi.pv243.ui;
 
 import javax.annotation.security.DeclareRoles;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.HttpConstraint;
 import javax.servlet.annotation.ServletSecurity;
@@ -9,18 +10,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 /**
  * Created by Michal Holic on 28/04/2016
  */
 @ServletSecurity(@HttpConstraint(rolesAllowed = {"admin", "artist", "reviewer"}))
 @DeclareRoles({"admin", "artist", "reviewer"})
-@WebServlet(name = "RootServlet", urlPatterns = {"/"}, loadOnStartup = 1)
-public class RootServlet extends HttpServlet {
+@WebServlet(name = "SecuredServlet", urlPatterns = {"/secured"}, loadOnStartup = 1)
+public class SecuredServlet extends HttpServlet {
+
+	private static final Logger log = Logger.getLogger(SecuredServlet.class.getName());
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().print("Hello from RootServlet");
+		if (req.isUserInRole("admin")) {
+			log.info("Admin logged in.");
+			resp.sendRedirect("/secured/admin/index.html");
+		}
+		else if (req.isUserInRole("reviewer")) {
+			log.info("Reviewer logged in.");
+			resp.sendRedirect("/secured/reviewer/index.html");
+		}
+		else  {
+			log.warning(req.getUserPrincipal().getName() + " failed to log in.");
+			resp.sendRedirect("/403.html");
+		}
 	}
 }
