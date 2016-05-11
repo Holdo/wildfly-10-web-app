ebbf07bfd99e67c6c3d47b2267eee2b6f4fa13db
Marián Čamák <majocamak@gmail.com>
2016-05-11 11:01:59 +0200

    Minor changes

--- db/src/main/java/dao/DemoDaoImpl.java
+++ db/src/main/java/dao/DemoDaoImpl.java
@@ -7,6 +7,8 @@ import org.infinispan.commons.api.BasicCache;
 import org.infinispan.query.Search;
 import org.infinispan.query.dsl.Query;
 import org.infinispan.query.dsl.QueryFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
@@ -32,6 +34,7 @@ import static javax.transaction.Transactional.TxType.SUPPORTS;
 @Transactional(REQUIRES_NEW)
 public class DemoDaoImpl implements DemoDAO {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass().getName());
     public static final String DEMO_CACHE_NAME = "demoCache";
 
     @Inject
@@ -71,7 +74,7 @@ public class DemoDaoImpl implements DemoDAO {
         Query q = qf.from(Demo.class)
                 .having("artist").like("%" + (interpret.isEmpty() ? "donotmatch" : interpret) + "%")
                 .toBuilder().build();
-        System.out.println("Infinispan Query DSL: " + q.toString());
+        log.info("Infinispan Query DSL: " + q.toString());
 
         return q.list().stream().filter(o -> o instanceof Demo).map(o -> (Demo) o).collect(Collectors.toList());
     }
