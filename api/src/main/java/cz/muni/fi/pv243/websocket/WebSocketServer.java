ea2a6cbeb0aa055a4f89e1d1baac593083410b6c
skylar <skyllarr@gmail.com>
2016-05-22 10:22:02 +0200

    websocket implemented

--- api/src/main/java/cz/muni/fi/pv243/websocket/WebSocketServer.java
+++ api/src/main/java/cz/muni/fi/pv243/websocket/WebSocketServer.java
@@ -4,31 +4,101 @@ package cz.muni.fi.pv243.websocket;
  *
  * @author skylar
  */
+import cz.muni.fi.pv243.dao.DemoDAO;
+import cz.muni.fi.pv243.model.Demo;
+import cz.muni.fi.pv243.model.Comment;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.json.Json;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 
-	
 @ApplicationScoped
-@ServerEndpoint("/actions")
+@ServerEndpoint("/comment")
 public class WebSocketServer {
-	@OnOpen
-		public void open(Session session) {
-	}
 
-	@OnClose
-		public void close(Session session) {
-	}
+    @Inject
+    private DemoDAO demoDao;
 
-	@OnError
-		public void onError(Throwable error) {
-	}
+    Map<String, List<Session>> rooms = new HashMap<>();
+    private Logger log = LoggerFactory.getLogger(this.getClass().getName());
 
-	@OnMessage
-		public void handleMessage(String message, Session session) {
-	}
+    @OnOpen
+    public void open(Session session) {
+    }
+
+    @OnClose
+    public void close(Session session) {
+        removeSession(session);
+    }
+
+    @OnError
+    public void onError(Throwable error) {
+        log.info("Error: " + error.getMessage());
+    }
+
+    @OnMessage
+    public void handleMessage(final Session session, final String message) {
+        JsonObject messageJson;
+        try (JsonReader jsonReader = Json.createReader(new StringReader(message))) {
+            messageJson = jsonReader.readObject();
+        }
+        Comment comment = new Comment();
+        comment.setAuthor(messageJson.getString("author"));
+        comment.setComment(messageJson.getString("comment"));
+        addCommentToDemo(messageJson.getString("title"), comment);
+
+        List<Session> sessions = rooms.get(messageJson.getString("title"));
+        JsonObject notification = Json.createObjectBuilder()
+                .add("title", messageJson.getString("title"))
+                .add("author", messageJson.getString("author"))
+                .add("comment", messageJson.getString("comment"))
+                .build();
+        sessions.stream().forEach((sessionForTitle) -> {
+            sendToSession(sessionForTitle, notification);
+        });
+    }
+
+    private void removeSession(Session session) {
+        rooms.keySet().stream().map((title)
+                -> rooms.get(title)).filter((sessionsOpenedForTitle)
+                -> (sessionsOpenedForTitle != null))
+                .filter((sessionsOpenedForTitle)
+                        -> (sessionsOpenedForTitle.contains(session)))
+                .forEach((sessionsOpenedForTitle) -> {
+                    sessionsOpenedForTitle.remove(session);
+                });
+    }
+
+    private void sendToSession(Session session, JsonObject message) {
+        if (session.isOpen()) {
+            try {
+                session.getBasicRemote().sendText(message.toString());
+            } catch (IOException ex) {
+                removeSession(session);
+                log.info("Error: " + ex.getMessage());
+            }
+        }
+    }
+
+    private void addCommentToDemo(String title, Comment comment) {
+        Demo demo = demoDao.findDemo(title);
+        List<Comment> comments = demo.getComments();
+        comments.add(comment);
+        demo.setComments(comments);
+        demoDao.updateDemo(demo);
+    }
 }
