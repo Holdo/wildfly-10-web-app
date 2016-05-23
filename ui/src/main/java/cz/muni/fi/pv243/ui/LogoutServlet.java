752c9bc26532714ecdd2aa9a5b71de3882a2e17e
Holdo <holic.michal@gmail.com>
2016-05-23 18:47:20 +0200

    FORM auth, logout, shell header

---
+++ ui/src/main/java/cz/muni/fi/pv243/ui/LogoutServlet.java
@@ -0,0 +1,26 @@
+package cz.muni.fi.pv243.ui;
+
+import javax.annotation.security.DeclareRoles;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.ServletSecurity;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Michal Holic on 23/05/2016
+ */
+@ServletSecurity(@HttpConstraint(rolesAllowed = {"admin", "reviewer"}))
+@DeclareRoles({"admin", "reviewer"})
+@WebServlet(name = "LogoutServlet", urlPatterns = {"/secured/logout"}, loadOnStartup = 1)
+public class LogoutServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.logout();
+		resp.sendRedirect("/");
+	}
+}
---
+++ ui/src/main/webapp/secured/admin/admininterface/App.controller.js
@@ -0,0 +1,28 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
+    var AppController =  Controller.extend("admininterface.App", {
+        onInit : function () {
+            var sUsername = getCookie("username");
+            if (sUsername != "") this.getView().byId("ShellHeadUserItem").setUsername(sUsername);
+        },
+        handleLogoffPress : function () {
+            window.location.href = "/secured/logout";
+        },
+        handlePressHome : function () {
+            window.location.href = "/";
+        }
+    });
+    return AppController;
+});
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
