feb93f6d5ce0191cd6bfe583ce25a4b3c53ed927
Michal Holič <holic.michal@gmail.com>
2016-05-30 13:50:34 +0200

    Fixed undeploy.sh

--- undeploy.sh
+++ undeploy.sh
@@ -1,3 +1,2 @@
 export JBOSS_HOME="`pwd`/wildfly-10.0.0.Final"
-
-sh $JBOSS_HOME/bin/jboss-cli.sh --connect --command="undeploy ui-1.0-SNAPSHOT.war"
+sh $JBOSS_HOME/bin/jboss-cli.sh --connect --command="undeploy --all-relevant-server-groups ui-1.0-SNAPSHOT.war"
